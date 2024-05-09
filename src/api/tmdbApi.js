989550c4553ca67df1b0d5957ee999dcb14e75b7
shivam shukla <134259762+shivamCoder25@users.noreply.github.com>
2024-05-09 09:23:37 +0530

    Add files via upload

---
+++ src/api/tmdbApi.js
@@ -0,0 +1,64 @@
+import axiosClient from "./axiosClient";
+
+export const category = {
+    movie: 'movie',
+    tv: 'tv',
+    top_rated: 'top_rated',
+    popular: 'popular',
+    top_ratedtv: 'top_ratedtv',
+    trendingtv: 'trendingtv',
+    live: 'live',
+    latest: 'latest'
+    
+}
+
+export const movieType = {
+    upcoming: 'upcoming',
+    popular: 'popular',
+    top_rated: 'top_rated',
+    now_playing: 'now_playing'
+    
+}
+
+export const tvType = {
+    popular: 'popular',
+    top_rated: 'top_rated',
+    on_the_air: 'on_the_air'
+}
+
+const tmdbApi = {
+    getMoviesList: (type, params) => {
+        const url = 'movie/' + movieType[type];
+        return axiosClient.get(url, params);
+    },
+    getTvList: (type, params) => {
+        const url = 'tv/' + tvType[type];
+        return axiosClient.get(url, params);
+    },
+    getVideos: (cate, id) => {
+        const url = category[cate] + '/' + id + '/videos';
+        return axiosClient.get(url, {params: {}});
+    },
+    search: ( params) => {
+        const url = 'search/multi';
+        return axiosClient.get(url, params);
+    },
+    detail: (cate, id, params) => {
+        const url = category[cate] + '/' + id;
+        return axiosClient.get(url, params);
+    },
+    credits: (cate, id) => {
+        const url = category[cate] + '/' + id + '/credits';
+        return axiosClient.get(url, {params: {}});
+    },
+    similar: (cate, id) => {
+        const url = category[cate] + '/' + id + '/similar';
+        return axiosClient.get(url, {params: {}});
+    },
+    reviews: (cate, id) => {
+        const url = category[cate] + '/' + id + '/reviews';
+        return axiosClient.get(url, {params: {}});
+    },
+}
+
+export default tmdbApi;
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import './footer.scss';
+
+import { Link } from 'react-router-dom';
+
+import bg from '../../assets/footer-bg.jpg';
+import logo from '../../assets/tmovie.png';
+
+const Footer = () => {
+    return (
+        <div className="footer" style={{backgroundImage: `url(${bg})`}}>
+            <div className="footer__content container">
+                <div className="footer__content__logo">
+                    <div className="logo">
+                        <img src={logo} alt="" />
+                        <Link to="/">PopcornTV</Link>
+                    </div>
+                </div>
+                <div className="footer__content__menus">
+                    <div className="footer__content__menu">
+                        <Link to="/">Home</Link>
+                    </div>
+                    <div className="footer__content__menu">
+                        <Link to="/live">Live</Link>
+                    </div>
+                    <div className="footer__content__menu">
+                        <Link to="/top_rated">Must watch</Link>
+                    </div>
+                    <div className="footer__content__menu">
+                        <Link to="/latest">Recent release</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
